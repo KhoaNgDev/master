a21ec1917fe73562a7dc03d28ab4a157cebc575a
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-07-01 15:14:58 +0700

    Create eloquent resource for transforming product records

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-        return inertia('Product/Index');
+    public function index()
+    {
+        $products = auth()->user()
+        ->products
+        ->latest()
+        ->get();
+
+        return inertia('Product/Index', [
+            'products' => ProductResource::collection($products)
+        ]);
     }
 }
